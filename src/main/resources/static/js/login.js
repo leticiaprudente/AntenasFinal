b4ae38a16b690a37b2dd081b140898e641533eeb
LeticiaPrudente <let.prudente@gmail.com>
2019-11-13 13:19:28 -0300

    verificação do e-mail institucional no cadastro do aluno

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -16,18 +16,50 @@ $(document).ready(function() {
 
 $('#form_register').submit(function(e){  
     e.preventDefault();
+  
     json = {
-           nome: $("#nome-cadastro").val(),
-           email: $("#email-cadastro").val(),
-           senha: $("#senha-cadastro").val(),
-           //Nivel em teste!!!
-           nivel: 1
-       }
-    jsonString = JSON.stringify(json);
-
-    $.post("/aluno-cadastro",jsonString,'json');
-    fechaPopupCadastro(event);
+    		nome: $("#nome-cadastro").val(),
+	        email: $("#email-cadastro").val(),
+	        senha: $("#senha-cadastro").val(),
+	        //Nivel em teste!!!
+	        nivel: 1
+    }
+	jsonString = JSON.stringify(json);
+    
+    
+    email= $("#email-cadastro").val();
+    let retornoValidacao = validaEmail(email);
+    if(retornoValidacao==true)
+	{
+	    $.post("/aluno-cadastro",jsonString,'json');
+	    fechaPopupCadastro(event);
+    }
+    else
+    {
+    	alert("O cadastro só pode ser realizado com seu e-mail institucional");
+    
+    }
 });
+
+
+function validaEmail(email) {
+    let texto = /^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/;
+    if (texto.test(email)) {
+        if (email.indexOf('@fatec.sp.gov.br', email.length - '@fatec.sp.gov.br'.length) !== -1) {
+            alert('Cadastro realizado com sucesso!');
+            return true;
+        } else {
+            alert('Use seu e-mail institucional para realizar o cadastro (seu.nome@fatec.sp.gov.br)');
+            return false;
+        }
+    } else {
+        alert('Endereço de e-mail inválido');
+        return false;
+    }
+}
+
+
+
 
 $('#form_login').submit(function(e){    
 				
